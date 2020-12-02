837017a6f8bce1436a40a7077a942c2a4a973a62
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-12-02 16:37:17 +0530

    Able to cast vote

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,44 @@ pragma solidity ^0.5.0;
 
 contract Election{
 
-    string public candidate;
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //accounts that have voted
+    mapping(address => bool) public voters;
+
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    
+    function addCandidate(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount]=Candidate(candidatesCount,_name,0);
+    }
+
+
 
 
     constructor () public{
-        candidate="Candidate 1";
+        addCandidate("Narendra Modi");
+        addCandidate("Rahul Gandhi");
+    }
+
+    function vote(uint _candidateId) public{
+
+        require(!voters[msg.sender]);
+
+        require(_candidateId>0 && _candidateId<=candidatesCount);
+        
+        //record that voter has voted
+        voters[msg.sender]=true;
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount++;
     }
 }
