9344956937dd11a2617d1c06b5f3f9d882dc551f
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-12-02 12:51:14 +0530

    Smoke test completed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Election = artifacts.require("./Election.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Election);
 };
